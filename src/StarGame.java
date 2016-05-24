d927fcca54c0be6e106879dfda1e7e198780d5a3
MahadevTW <mahadev@thoughtworks.com>
2016-05-24 08:26:47 +0530

    vertical Line stars

--- src/StarGame.java
+++ src/StarGame.java
@@ -1,11 +1,18 @@
-/**
- * Created by mahadev on 24/05/16.
- */
 public class StarGame {
     public static void main(String[] args) {
         simpleStar();
         int numberOfStars = 8;
         horizontalLine(numberOfStars);
+        System.out.println();
+        System.out.println("Vertical Stars");
+        numberOfStars = 3;
+        verticalLine(numberOfStars);
+    }
+
+    private static void verticalLine(int numberOfStars) {
+        for (int i = 0; i < numberOfStars; i++) {
+            System.out.println("*");
+        }
     }
 
     private static void horizontalLine(int numberOfStars) {
