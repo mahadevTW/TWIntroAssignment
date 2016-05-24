b63a614928f41ec008afe0fb927dfbb29ce5e868
MahadevTW <mahadev@thoughtworks.com>
2016-05-24 08:22:17 +0530

    Horizontal line Printed

--- src/StarGame.java
+++ src/StarGame.java
@@ -4,6 +4,14 @@
 public class StarGame {
     public static void main(String[] args) {
         simpleStar();
+        int numberOfStars = 8;
+        horizontalLine(numberOfStars);
+    }
+
+    private static void horizontalLine(int numberOfStars) {
+        for (int i = 0; i < numberOfStars; i++) {
+            System.out.print("*");
+        }
     }
 
     private static void simpleStar() {
