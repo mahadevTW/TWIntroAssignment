7f21a2c5a21806900c7c783e275be64a30ed9f66
MahadevTW <mahadev@thoughtworks.com>
2016-05-24 08:30:08 +0530

    Right trangle

--- src/StarGame.java
+++ src/StarGame.java
@@ -7,6 +7,17 @@ public class StarGame {
         System.out.println("Vertical Stars");
         numberOfStars = 3;
         verticalLine(numberOfStars);
+        System.out.println("Right Angle trangle");
+        rightTrangle(numberOfStars);
+    }
+
+    private static void rightTrangle(int numberOfStars) {
+        for (int i = 0; i < numberOfStars; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     private static void verticalLine(int numberOfStars) {
