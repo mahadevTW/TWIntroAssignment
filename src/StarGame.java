72d89a56e05d0b8ef3b9157e422af85f487d0e13
MahadevTW <mahadev@thoughtworks.com>
2016-05-24 08:19:29 +0530

    simple Star functio

---
+++ src/StarGame.java
@@ -0,0 +1,12 @@
+/**
+ * Created by mahadev on 24/05/16.
+ */
+public class StarGame {
+    public static void main(String[] args) {
+        simpleStar();
+    }
+
+    private static void simpleStar() {
+        System.out.println("*");
+    }
+}
